bc22482d83449da9a9bb9bf9feea19a4222ebc58
David <david@cuisine.home>
2018-10-10 23:17:50 +0200

    Test

---
+++ index.js
@@ -0,0 +1,60 @@
+// Creating an express server
+
+var express = require('express'),
+	app = express();
+
+// This is needed if the app is run on heroku and other cloud providers:
+
+var port = process.env.PORT || 8080;
+
+// Initialize a new socket.io object. It is bound to
+// the express app, which allows them to coexist.
+
+var io = require('socket.io').listen(app.listen(port));
+
+// Initialize JSON Web Token Simple module https://www.npmjs.com/package/jwt-simple
+var jwt = require('jwt-simple');
+var secret = Buffer.from('thisisdoublescreenapp', 'hex');
+
+// App Configuration
+
+// Make the files in the public folder available to the world
+app.use(express.static(__dirname + '/public'));
+
+var presentation = io.on('connection', function (socket) {
+
+//Code generator
+  socket.on('getToken', function(data){
+    socket.emit('token', {
+      code: data.code,
+      room: data.room,
+      token: jwt.encode(data.room, secret)
+    });
+  });
+
+//Code listener
+  socket.on('connectToRoom', function(data){
+    var sentToken = jwt.decode(data.token, secret);
+    socket.join(sentToken);
+    io.to(sentToken).emit('console', {
+      msg: 'connected to ' + sentToken + ' as ' + data.role
+    });
+    if(data.role == 'slave'){
+      io.to(sentToken).emit('redirect');
+    }
+  });
+
+  socket.on('action', function(data){
+    var sentToken = jwt.decode(data.token, secret);
+    io.to(sentToken).emit('doThat', {
+      emitter: data.emitter,
+      action: data.action,
+      target: data.target
+    });
+  });
+
+});
+
+
+// Tell the app is running
+console.log('Your presentation is running on http://localhost:' + port);
---
+++ public/script.js
@@ -0,0 +1,158 @@
+$(function(){
+  if(Cookies.get('token') || Cookies.get('role')){
+    Cookies.remove('token');
+    Cookies.remove('role');
+  }
+
+  var socket = io();
+  var room = Date.now().toString();
+  var code = room.substring(room.length-6, room.length);
+
+//
+  $('#generateButton').click(function(){
+    socket.emit('getToken', {
+    			room: room,
+          code: code
+    });
+    socket.on('token', function(data){
+    $('body').append('<div class="message"><span class="instructions">Ouvrez ce site dans un autre navigateur et entrez le code :</span><span class="important"> ' + code + '</span></div>')
+    Cookies.set('token', data.token);
+    Cookies.set('role', 'master');
+    if(Cookies.get('token')){
+      socket.emit('connectToRoom', {
+        token: Cookies.get('token'),
+        role: 'master'
+      });
+
+    }
+    });
+  })
+
+//
+  $('.login').submit(function(e){
+
+		e.preventDefault();
+
+		key = $('#enterCode').val().trim();
+
+		if(key.length) {
+			var token = socket.emit('getToken', {
+        room: room.substring(0, room.length-6)+key,
+        code: key
+			});
+      socket.on('token', function(data){
+        Cookies.set('token', data.token);
+        Cookies.set('role', 'slave');
+      });
+      if(Cookies.get('token')){
+        socket.emit('connectToRoom', {
+          token: Cookies.get('token'),
+          role: 'slave'
+        });
+      }
+    }
+
+	});
+
+//
+$('.controls').on('click', '.play', function(){
+  socket.emit('action',{
+    token: Cookies.get('token'),
+    emitter: Cookies.get('role'),
+    action: 'playVideo',
+    target: 'activeVideo'
+  });
+});
+
+//
+$('.controls').on('click', '.pause', function(){
+  socket.emit('action',{
+    token: Cookies.get('token'),
+    emitter: Cookies.get('role'),
+    action: 'pauseVideo',
+    target: 'activeVideo'
+  });
+});
+
+
+//
+$('.activeVideo').on('timeupdate', function() {
+
+  function changeDesc(target){
+    socket.emit('action',{
+      token: Cookies.get('token'),
+      emitter: Cookies.get('role'),
+      action: 'showDesc',
+      target: target
+    });
+  }
+console.log(Math.floor($(this)[0].currentTime));
+    switch(Math.floor($(this)[0].currentTime)){
+      case 10:
+      changeDesc('donetsk');
+      break;
+
+      case 43:
+      changeDesc('dnr');
+      break;
+
+      case 87:
+      changeDesc('hryvnia');
+      break;
+
+      case 125:
+      changeDesc('yassynouvata');
+      break;
+
+      default:
+      break;
+    }
+
+});
+
+//
+socket.on('doThat', function(data){
+  switch(data.action){
+    case 'playVideo':
+    if(data.emitter!==Cookies.get('role')){
+      $('.'+data.target).get(0).play();
+    } else {
+      $('.play').toggleClass('play pause');
+    }
+    break;
+
+    case 'pauseVideo':
+    if(data.emitter!==Cookies.get('role')){
+      $('.'+data.target).get(0).pause();
+    } else {
+      $('.pause').toggleClass('play pause');
+    }
+    break;
+
+    case 'showDesc':
+    if(data.emitter!==Cookies.get('role') && data.target !== Cookies.get('activeDesc')){
+      Cookies.set('activeDesc', data.target)
+      $('.description').fadeOut('slow').delay(1000);
+      $('.'+data.target).fadeIn('slow');
+    } else {
+    }
+    break;
+
+    default:
+    break;
+  }
+});
+
+//
+  socket.on('redirect', function(data){
+    $('.login, .message').hide();
+    var role = Cookies.get('role');
+    $('.'+role).fadeIn('slow');
+  });
+
+//
+  socket.on('console', function(data){
+    console.log(data);
+  });
+
+})
